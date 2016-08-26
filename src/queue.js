7115377a1186231a517fb241d192f6ea48f7196a
Влад Тарасенко < >
2016-08-26 17:39:35 +0300

    queue is almost done(except shift. 54 from 56)

--- src/queue.js
+++ src/queue.js
@@ -4,21 +4,23 @@ class PriorityQueue {
 	constructor(maxSize) {
 this.maxSize=maxSize||30;
 this.heap=new MaxHeap();
-this.queueSize=0;
 	}
 
 	push(data, priority) {
+		if(this.size()>=this.maxSize)
+		throw new Error("Queue has max size");
 		this.heap.push(data,priority);
-// if(this.size()>=this.maxSize)
-// throw new Error("Queue has max size");
 	}
 
 	shift() {
-
+		if(this.isEmpty())
+			throw new Error("Queue is empty");
+ 		var value=this.heap.pop();
+ 		return value;
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
